19901f66bf718b5eb3cb29a39769a0a800e2e1da
Evgeniy <palama15@mail.ru>
2023-11-17 00:09:51 +0500

    Initial

---
+++ src/Main.java
@@ -0,0 +1,46 @@
+public class Main {
+
+    public static void main(String[] args) {
+        BMIService service = new BMIService();
+        int weight = 198; //масса в кг
+        int height = 200; //рост в см
+
+        String bodyMassIndex;
+
+        bodyMassIndex = service.calculate(weight, height);
+        System.out.println(bodyMassIndex);
+
+        bodyMassIndex = service.calculate(0, 180);
+        System.out.println(bodyMassIndex);
+
+        bodyMassIndex = service.calculate(251, 180);
+        System.out.println(bodyMassIndex);
+
+        bodyMassIndex = service.calculate(80, 50);
+        System.out.println(bodyMassIndex);
+
+        bodyMassIndex = service.calculate(80, 251);
+        System.out.println(bodyMassIndex);
+
+        bodyMassIndex = service.calculate( 50, 190);
+        System.out.println(bodyMassIndex);
+
+        bodyMassIndex = service.calculate( 60, 190);
+        System.out.println(bodyMassIndex);
+
+        bodyMassIndex = service.calculate( 70, 190);
+        System.out.println(bodyMassIndex);
+
+        bodyMassIndex = service.calculate( 100, 190);
+        System.out.println(bodyMassIndex);
+
+        bodyMassIndex = service.calculate( 120, 190);
+        System.out.println(bodyMassIndex);
+
+        bodyMassIndex = service.calculate( 140, 190);
+        System.out.println(bodyMassIndex);
+
+        bodyMassIndex = service.calculate( 160, 190);
+        System.out.println(bodyMassIndex);
+    }
+}
